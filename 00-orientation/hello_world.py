b9f3aa9bbaab1decb9c3cdd822b89f43e482893a
flccdave <157420500+flccdave@users.noreply.github.com>
2025-01-29 19:45:52 +0000

    First programgit add .!

---
+++ 00-orientation/hello_world.py
@@ -0,0 +1,40 @@
+# COMMENTS: Three times to use them
+#   1. Put name, date, purpose of program (declarative)
+#   2. TO DO list
+#   3. Make complicated code easier to understand
+
+# Dave Ghidiu
+# 2025-01-29
+# This program will display "Hello world" on the screen
+
+
+
+# TODO
+# Add blank lines above and below
+
+
+print('3 + 5')
+print(3 + 5)
+
+# DO NOT WASTE BRAIN CELLS LEARNING sep
+print('$3 + $5 = $', 3 + 5, 'tada!' , sep='')
+print('Hello world!', 'Dave')
+
+print('Hello world!') # String Literal 
+print()
+
+
+# # This code asks the user for a number
+# number = int(input("Enter a number: "))
+
+# # We will use MOD to determine if the number is even or odd
+# if number %2 == 0:
+
+#     # Print "even"
+#     print('even')
+
+# else:
+#     # Print "odd"
+#     print('odd')
+
+
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # flcc-python
+
+Dave
