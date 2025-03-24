78d9eaf5d60d35fab5a154b48bd86e285e50e9e7
flccdave <157420500+flccdave@users.noreply.github.com>
2025-03-24 18:20:20 +0000

    Loops

---
+++ 05-Loops/for_loops_demo.py
@@ -0,0 +1,77 @@
+# doggo_names = ['BB-8', 'Quinnie', 'Maggie']
+
+# for dog in doggo_names:
+#     print(dog)
+
+
+# for i in range(10):
+#     print(i)
+
+# for i in range(1, 11):
+#     print(i)
+
+# for i in range(7, 43, 8):
+#     print(i)
+
+start_number = int(input('Start? '))
+end_number = int(input('End? '))
+up_by = int(input('Up by? '))
+
+for i in range(start_number, end_number, up_by):
+    print(i)
+
+
+
+
+
+
+# password = input('Please enter your password: ')
+# print(password)
+
+# print()
+
+# lowercase_count = 0
+# uppercase_count = 0
+
+# for character in password:
+    
+#     if (character >= 'a') and (character <= 'z'):
+#         lowercase_count = lowercase_count + 1
+#         # print('lowercase letter found')
+  
+#     if (character >= 'A') and (character <= 'Z'):
+#         uppercase_count = uppercase_count + 1
+
+
+# print(f'Lower case: {lowercase_count}')
+# print(f'Upper case: {uppercase_count}')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ 05-Loops/while_loops_demo.py
@@ -0,0 +1,30 @@
+start_number = int(input("What number do you want to start at? "))
+end_number = int(input("What would you like to count up to? "))
+go_up_by = float(input("What would you like to go up by? "))
+
+while start_number <= end_number:
+    print(start_number)
+    start_number = start_number + go_up_by
+
+
+print()
+print()
+print()
+
+
+another_compliment = 'DRG'
+
+while another_compliment.lower() != 'n':
+    print('You have nice eyes!')
+    another_compliment = input('Do you want another compliment (y/n)? ')
+
+
+
+print('\n\n\nGAME OVER MAN!!!!')
+
+
+
+
+
+
+
